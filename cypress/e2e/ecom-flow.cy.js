c8ba62c0876b2a9b868814e7dbe260d9aed1272d
Allan Magtibay Jr. <amagtibay@vertere-gs.com>
2025-04-08 16:48:47 +0800

    Test: Added ecom-flow.cy.js file that tests E-commerce work flow. Also added commands in commands.js file

---
+++ cypress/e2e/ecom-flow.cy.js
@@ -0,0 +1,50 @@
+describe('E-commerce Workflow Suite.', () => {
+  beforeEach(() => {
+    cy.visit("https://www.saucedemo.com/",  {timeout: 240000});
+  })
+
+  it("Should Successfully Login", () => {
+    cy.auth('standard_user', 'secret_sauce');
+
+    // Assert successful login by checking URL
+    cy.url().should("include", "/inventory.html");
+
+    // Assert the presence of a logged-in indicator (like the shopping cart)
+    cy.get(".shopping_cart_link").should("be.visible");
+  
+    // Screenshot
+    cy.takeScreen("login");
+  });
+
+  it("Should Successfully Add to Cart", () => {
+    cy.auth('standard_user', 'secret_sauce');
+
+    // Add item to cart
+    cy.addToCart("Sauce Labs Backpack");
+
+    /// Screenshot
+    cy.takeScreen("add-to-cart");
+  });
+
+  it("Should Successfully Checkout", () => {
+    cy.auth('standard_user', 'secret_sauce');
+
+    // Add item to cart
+    cy.addToCart("Sauce Labs Backpack");
+
+    // Go to cart
+    cy.get('.shopping_cart_link').click();
+    cy.url().should("include", "/cart.html"); // Check if in cart.html site
+
+    // Checkout
+    cy.checkout();
+   
+    // Assert success message
+    cy.get('[data-test="checkout-complete-container"]') // Get test checkout container
+    .contains("Thank you for your order!") // Contains thank you message
+    .should("be.visible"); // Should be visible
+    
+    // Screenshot
+    cy.takeScreen("checkout");
+  })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,41 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// Add to Cart Command
+Cypress.Commands.add("addToCart", (itemName) => {
+    cy.get('[data-test="inventory-item-name"]') // Selects product names
+    .contains(itemName) // Finds the correct item by name
+    .parents(".inventory_item") // Moves up to the item's container
+    .find("button") // Locates the "Add to Cart" button
+    .click();
+});
+
+Cypress.Commands.add("checkout", () => {
+    cy.get('[data-test="checkout"]').click();
+
+     // Enter checkout info
+     cy.get('[data-test="firstName"]').type("John Mark Arman Carl Johna Stephen Nolan Eren");
+     cy.get('[data-test="lastName"]').type("Dimagiba Dela Cruz");
+     cy.get('[data-test="postalCode"]').type("123321");
+     cy.get('[data-test="continue"]').click();
+ 
+     // Finish order
+     cy.get('[data-test="finish"]').click();
+
+});
+
+// Checkout Screenshot Command
+Cypress.Commands.add("takeScreen", (prefix) => {
+    const date = new Date();
+    const formattedDate = `${String(date.getMonth()+1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}-${String(date.getFullYear()).slice(-2)}`;
+    const fileName = `${prefix}-${formattedDate}`;
+    cy.screenshot(fileName);
+});
+  
+// Auth function
+Cypress.Commands.add("auth", (username, password) => {
+    cy.get('[data-test="username"]').type(username);
+    cy.get('[data-test="password"]').type(password);
+    cy.get('[data-test="login-button"]').click();
+});
