06a1d2643a15173927744ed6a58bbf933cfb3f1d
Allan Magtibay Jr. <amagtibay@vertere-gs.com>
2025-04-08 10:29:35 +0800

    Edited cypress.yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
+// npx cypress run --record --key 22e972df-2261-4a3d-a8cc-8feb1c4531ca
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "7h4a3j",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
