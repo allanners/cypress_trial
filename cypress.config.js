6c5bf352c108377ca855b98341e52657a8f9f251
Allan Magtibay Jr. <amagtibay@vertere-gs.com>
2025-04-15 13:39:46 +0800

    Updated ecom-flow.cy.js

--- cypress.config.js
+++ cypress.config.js
@@ -14,7 +14,7 @@ module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     charts: true,
-    reportPageTitle: 'custom-title',
+    reportPageTitle: 'Test Report',
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
